f6adb77df8c6678cb7ff723c908ab1721c327782
John Martin <curmudgeon@rallydev.com>
2012-10-30 14:42:31 -0700

    First pass

---
+++ App.js
@@ -0,0 +1,76 @@
+/*global console, Ext */
+var renderUSDate = function(value) {
+    var display_value = "";
+    if ( value ) {
+        display_value = Rally.util.DateTime.format( value, 'm/d/Y');
+    }
+    return display_value;
+};
+var renderFieldName = function(value) {
+    var display_value = "";
+    if ( value ) {
+        display_value = value._refObjectName;
+    }
+    return display_value;
+};
+    
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    field_to_sum: 'FeaturePointEstimate',
+    columns: [
+        { text: 'ID', dataIndex: 'FormattedID' },
+        { text: 'Name', dataIndex: 'Name', flex: 1 },
+        { text: 'Running Total', dataIndex: 'RunningTotal' },
+        { text: 'Feature Point Estimate', dataIndex: 'FeaturePointEstimate' },
+        { text: 'Last Update', dataIndex: 'LastUpdateDate', renderer: renderUSDate },
+        { text: 'Planned Start', dataIndex: 'PlannedStartDate', renderer: renderUSDate },
+        { text: 'Planned End', dataIndex: 'PlannedEndDate', renderer: renderUSDate },
+        { text: 'Major Release', dataIndex: 'MajorRelease' },
+        { text: 'State', dataIndex: 'State', renderer: renderFieldName },
+        { text: 'Owner', dataIndex: 'Owner', renderer: renderFieldName },
+        { text: 'Parent', dataIndex: 'Parent', renderer: renderFieldName }
+    ],
+    componentCls: 'app',
+    items: [{ xtype: 'container', itemId: 'grid_box', height: 500 }],
+    launch: function() {
+    	this._getPIModel();
+    },
+    _getPIModel: function() {
+    	Rally.data.ModelFactory.getModel({
+    		type: 'PortfolioItem/Feature',
+    		success: function(userStoryModel) {
+    			this.model = userStoryModel;
+    			this._addPIGrid();
+    		},
+    		scope: this
+    	});
+    },
+    _addPIGrid: function() {
+    	if ( this.model ) {
+    		this.pigrid = Ext.create( 'Rally.ui.grid.Grid', {
+    			model: this.model,
+    			enableRanking: true,
+	    		columnCfgs: this.columns,
+                storeConfig: {
+                    sorters: [ { property: 'Rank', direction: 'ASC' } ],
+                    listeners: {
+                        datachanged: function( store, opts ) {
+                            console.log( "datachanged", store.getRecords() );
+                            var records = store.getRecords();
+                            var running_total = 0;
+                            for ( var i=0; i<records.length; i++ ) {
+                                var record = records[i];
+                                var value = record.data[this.field_to_sum] || 0;
+                                running_total += value;
+                                record.data.RunningTotal = running_total;
+                            }
+                        },
+                        scope: this
+                    }
+                }
+
+    		});
+	    	this.down('#grid_box').add(this.pigrid);
+    	}
+    }
+});
--- README.md
+++ README.md
@@ -2,3 +2,9 @@ running-total-pi-grid
 =====================
 
 Running Total Portfolio Item Grid
+
+This is an example of how I made a column that did a running total in a rally grid. There might be other ways to do this, so feedback is welcome.
+
+This assumes a custom field for the feature level of PIs is created called "Feature Point Estimate".  This is summed and put into a column for RunningTotal.  RunningTotal does not have to exist as a field.
+
+Video: http://screencast.com/t/ojo4XSdei
