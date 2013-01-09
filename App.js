4a1eb48a35c7b32c9cb3da96f6ca852b8c0d01f0
John Martin <curmudgeon@rallydev.com>
2013-01-08 18:14:16 -0800

    add a date filter

--- App.js
+++ App.js
@@ -18,27 +18,56 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     field_to_sum: 'FeatureEstimate',
     componentCls: 'app',
-    items: [{ xtype: 'container', itemId: 'grid_box' }],
+    items: [
+        { xtype: 'container',  layout: { type: 'hbox' }, items: [ 
+            { xtype: 'container', margin: 5, itemId: 'date_box' }, 
+            { xtype: 'container', margin: 5, itemId: 'button_box' } 
+        ]},
+        { xtype: 'container', itemId: 'grid_box' }
+    ],
     launch: function() {
         window.console && console.log( "launch" );
+        this._addDatePicker();
+        this._addPrintButton();
         this._setColumns();
     	this._getPIModel();
     },
     _setColumns: function() {
         this.columns = [
 	        { xtype: 'rallyrankcolumn' },
-	        { text: 'ID', dataIndex: 'FormattedID' },
-	        { text: 'Name', dataIndex: 'Name', editor: 'rallytextfield', flex: 1 },
-	        { text: 'Running Total', dataIndex: 'RunningTotal' },
-	        { text: 'Feature Estimate', dataIndex: 'FeatureEstimate', editor: 'rallynumberfield' },
-	        { text: 'Planned Start', dataIndex: 'PlannedStartDate', editor: 'rallydatefield', renderer: renderUSDate },
-	        { text: 'Planned End', dataIndex: 'PlannedEndDate', editor: 'rallydatefield', renderer: renderUSDate },
-	        { text: 'State', dataIndex: 'State', editor: this._getStateEditor(), renderer: renderFieldName },
-	        { text: 'Owner', dataIndex: 'Owner', renderer: renderFieldName },
-	        { text: 'Parent', dataIndex: 'Parent', renderer: renderFieldName }
+	        { text: 'ID', dataIndex: 'FormattedID', flex: 0.25 },
+	        { text: 'Name', dataIndex: 'Name', editor: 'rallytextfield', flex: 2.5 },
+	        { text: 'Running Total', dataIndex: 'RunningTotal', flex: 0.5  },
+	        { text: 'Feature Estimate', dataIndex: 'FeatureEstimate', editor: 'rallynumberfield', flex: 0.5 },
+	        { text: 'Planned Start', dataIndex: 'PlannedStartDate', editor: 'rallydatefield', renderer: renderUSDate, flex: 0.75 },
+	        { text: 'Planned End', dataIndex: 'PlannedEndDate', editor: 'rallydatefield', renderer: renderUSDate, flex: 0.75 },
+	        { text: 'State', dataIndex: 'State', editor: this._getStateEditor(), renderer: renderFieldName, flex: 1.25 },
+	        { text: 'Owner', dataIndex: 'Owner', renderer: renderFieldName, flex: 0.75 },
+	        { text: 'Parent', dataIndex: 'Parent', renderer: renderFieldName, flex: 2 }
 	    ];
     },
-    
+    _addDatePicker: function() {
+        window.console && console.log( "_addDatePicker" );
+        this.date_picker = Ext.create( 'Rally.ui.DateField',{
+            fieldLabel: 'Planned Items After ',
+            labelWidth: 125,
+            value: Rally.util.DateTime.add( new Date(), "month", -1 ),
+            listeners: {
+                change: function( field, newValue, oldValue, eOpts ) {
+                    this._getData();
+                },
+                scope: this
+            }
+        } );
+        this.down("#date_box").add(this.date_picker);
+    },
+    _addPrintButton: function() {
+        window.console && console.log( "_addPrintButton" );
+        this.down('#button_box').add(Ext.create('Rally.ui.Button', { 
+            text: 'Do Nothing'
+        }));
+        
+    },
     _getStateEditor: function() {
         var editor = {
             xtype: 'rallycombobox',
@@ -73,9 +102,20 @@ Ext.define('CustomApp', {
     },
     _getData: function() {
     	var that = this;
+        window.console && console.log( this.date_picker.getValue() );
+        var filters = [ { property: 'ObjectID', operator: '>', value: '0' }];
+        if ( this.date_picker.getValue() ) {
+            var iso_value = Rally.util.DateTime.toIsoString( this.date_picker.getValue() );
+            filters = Ext.create('Rally.data.QueryFilter',
+                { property: 'PlannedEndDate', operator: '=', value: "null" }).or( Ext.create( 'Rally.data.QueryFilter',
+                { property: 'PlannedEndDate', operator: '>', value: iso_value }));
+            window.console && console.log( filters.toString() );
+            
+        }
     	this.pi_store = Ext.create('Rally.data.WsapiDataStore', {
     		autoLoad: true,
     		model: that.model,
+            filters: filters,
             sorters: [ { property: 'Rank', direction: 'ASC' } ],
     		listeners: {
     			load: function(store,data,success) {
@@ -112,21 +152,22 @@ Ext.define('CustomApp', {
     	if ( this.model ) {
             if ( this.pi_grid ) { 
                 window.console && console.log( "Already has a pi_grid" );
-            } else {
-	    		this.pi_grid = Ext.create( 'Rally.ui.grid.Grid', {
-	    			model: this.model,
-	    			height: 500,
-	    			/*enableRanking: true,*/
-	    			viewConfig: {
-	    				plugins: [
-	    					{ ptype: 'rallydragdrop2' }
-	    				]
-	    			},
-		    		columnCfgs: this.columns,
-		    		store: this.pi_store
-	    		});
-	    	  this.down('#grid_box').add(this.pi_grid);
-            }
+                this.pi_grid.destroy();
+            } 
+    		this.pi_grid = Ext.create( 'Rally.ui.grid.Grid', {
+    			model: this.model,
+    			height: 475,
+    			/*enableRanking: true,*/
+    			viewConfig: {
+    				plugins: [
+    					{ ptype: 'rallydragdrop2' }
+    				]
+    			},
+	    		columnCfgs: this.columns,
+	    		store: this.pi_store
+    		});
+    	    this.down('#grid_box').add(this.pi_grid);
+            
     	}
     }
 });
